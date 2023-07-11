4c6cbd86b2bc318908929778fe9a8727fa02d5ef
Munana Clinton <munanaclinton@gmail.com>
2023-07-11 20:21:06 +0200

    Add new data to the data sql

--- data.sql
+++ data.sql
@@ -4,4 +4,7 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered, weight_kg) VALUES (2, 'Gabumon', 'Nov-15-2018', 2, true, 8);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered, weight_kg) VALUES (3, 'Pikachu', 'Jan-7-2021', 1, false, 15.04);
 INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered, weight_kg) VALUES (3, 'Devimon', 'May-12-2017', 5, true, 11);
-
+INSERT INTO animals VALUES(5,'Charmander', 'FEB-8-2020', 0,false, -11),(6, 'Plantmon', 'NOV-15-2021',2,true,-5.7),
+(7,'Squirtle', 'APR-2-1993', 3, false,-12.13),(8, 'Angemon', 'jun-12-2005',1,true, -45);
+INSERT INTO animals VALUES(9, 'Boarmon', 'jun-7-2005', 7, true, 20.4),(10,'Blossom', 'OCT-13-1998', 3, true, 17),
+(11, 'Ditto', 'May-14-2022', 4, true, 22);
