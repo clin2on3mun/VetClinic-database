fda4c5d58b203310c263427355c0d71bdc08f8cd
Munana Clinton <munanaclinton@gmail.com>
2023-07-17 18:28:39 +0200

    Add more queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -151,5 +151,9 @@ WHERE vt.name ='Maisy Smith'
 GROUP BY species.name
 LIMIT 1;
 
+-- EXPLAIN ANALYZE QUERY 
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
